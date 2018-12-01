c4f54b6a727dc43503a359a9e69be00bcf2c0259
Антон Мещеряков <anton23.pochta@gmail.com>
2018-12-02 02:56:35 +0500

    Сделал урок №19

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready( function(){
+  $('#button-callback').on('click', function() {
+    $('#modal-callback').toggleClass('modal_visible')
+
+  });
+  $('.modal__close').on('click', function() {
+    $('#modal-callback').toggleClass('modal_visible')
+
+  });
+
+});
